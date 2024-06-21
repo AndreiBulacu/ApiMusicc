e1be8217c9a10a3979db61d0897b39be77364c52
Andrei Bulacu <andreihrc@yahoo.com>
2024-06-21 16:58:52 +0300

    Added basic CRUD operations for Songs list.

---
+++ ApiMusic/Controllers/SongsController.cs
@@ -0,0 +1,41 @@
+﻿using ApiMusic.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMusic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private static List<Song> songs = new List<Song>()
+        {
+            new Song(){Id=0, Title="Willow",Language="English"},
+            new Song(){Id=1, Title="After Glow",Language="English"},
+        };
+
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return songs;
+        }
+
+        [HttpPost]
+        public void Post ([FromBody]Song song)
+        {
+            songs.Add(song);
+        } 
+
+        [HttpPut("{id}")]
+        public void Put (int id, [FromBody]Song song)
+        {
+            songs[id] = song;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            songs.RemoveAt(id);
+        }
+    }
+}
