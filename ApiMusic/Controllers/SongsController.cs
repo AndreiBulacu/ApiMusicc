23d68de3381cfaab129ebf7a2e15f9bbc57e2ad9
Andrei Bulacu <andreihrc@yahoo.com>
2024-06-22 13:28:17 +0300

    Implemented Status Codes for SongsController.

--- ApiMusic/Controllers/SongsController.cs
+++ ApiMusic/Controllers/SongsController.cs
@@ -18,43 +18,47 @@ namespace ApiMusic.Controllers
 
         // GET: api/<SongsController>
         [HttpGet]
-        public IEnumerable<Song> Get()
+        public IActionResult Get()
         {
-            return _dbContext.Songs;
+            //return _dbContext.Songs;
+            return Ok(_dbContext.Songs);
         }
 
         // GET api/<SongsController>/5
         [HttpGet("{id}")]
-        public Song Get(int id)
+        public IActionResult Get(int id)
         {
-            return _dbContext.Songs.Find(id);
+            return Ok(_dbContext.Songs.Find(id));
         }
 
         // POST api/<SongsController>
         [HttpPost]
-        public void Post([FromBody] Song song)
+        public IActionResult Post([FromBody] Song song)
         {
             _dbContext.Songs.Add(song);
             _dbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
         }
 
         // PUT api/<SongsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Song songObj)
+        public IActionResult Put(int id, [FromBody] Song songObj)
         {
             var song = _dbContext.Songs.Find(id);
             song.Title = songObj.Title;
             song.Language = songObj.Language;
             _dbContext.SaveChanges();
+            return Ok("Record updated successfully");
         }
 
         // DELETE api/<SongsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
             var song = _dbContext.Songs.Find(id);
             _dbContext.Songs.Remove(song);
             _dbContext.SaveChanges();
+            return Ok("Record deleted");
         }
     }
 }
