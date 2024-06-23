dc2a519e653f955dad690b974842b613831a751b
Andrei Bulacu <andreihrc@yahoo.com>
2024-06-23 22:38:49 +0300

    Created application models.

--- ApiMusic/Models/Song.cs
+++ ApiMusic/Models/Song.cs
@@ -1,10 +1,27 @@
-﻿namespace ApiMusic.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiMusic.Models
 {
     public class Song
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Title cannot be null or empty.")]
         public string? Title { get; set; }
+        [Required(ErrorMessage = "Language cannot be null or empty.")]
         public string? Language { get; set; }
+        [Required(ErrorMessage = "Kindly provide the song duration.")]
         public string? Duration { get; set; }
+        public DateTime UploadedDate { get; set; }
+        public bool IsFeatured { get; set; }
+
+        [NotMapped]
+        public IFormFile? Image { get; set; }
+        public string? ImageUrl { get; set; }
+        [NotMapped]
+        public IFormFile? AudioFile { get; set; }
+        public string? AudioUrl { get; set; }
+        public int ArtistId { get; set; }
+        public int? AlbumId { get; set; }
     }
 }
